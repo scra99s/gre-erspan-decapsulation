a107629f4fb359a947348202e54afc233f8b1453
scra99s <christian.kneale@outlook.com.au>
2020-02-17 19:40:22 +1030

    testing

--- greauto.py
+++ greauto.py
@@ -2,16 +2,16 @@
 import os
 import subprocess
 
-def Select_Interface():
-  interfaceList = os.listdir('/sys/class/net/')
-  interfaceList.remove('lo')
+def Select_Interface(interfaceList = []):
   for num, name in enumerate(interfaceList):
-    print("%d) %s" % (num, name))
+    print(" %d) %s" % (num, name))
   interfaceNum = input('Please Select an interface for input:') 
   if isinstance(interfaceNum, int):
     if interfaceNum >= 0 and interfaceNum <= interfaceList.len():
-      happy
+      return interfaceNumber
   else:
     print('Invalid input!')
 
-Select_Interface()
+interfaceList = os.listdir('/sys/class/net/')
+interfaceList.remove('lo')
+Select_Interface(interfaceList)
