7b446b839758b85f29e7eb94d31ac595c5a54c1a
scra99s <christian.kneale@outlook.com.au>
2020-02-17 18:35:32 +1030

    testing

--- greauto.py
+++ greauto.py
@@ -4,3 +4,6 @@ import subprocess
 result = subprocess.run(['ip -br addr|cut -d" " -f1'], stdout=subprocess.PIPE)
 
 interfaceList = result.stdout.decode('utf-8').splitlines())
+interfaceList.remove('lo')
+
+print(interfaceList)
